5fc04b79b36db0e94828fdfb48d17cd4fd54a926
Dylan Yung <YungDexiong@hotmail.com>
2020-02-04 00:51:34 -0800

    fb firstname added

--- script.py
+++ script.py
@@ -1,23 +1,18 @@
 import pandas as pd 
 import os
 
-files = os.listdir("namesbystate/")
-
 df = pd.DataFrame(columns=['count', 'name'])
 
+f = open("facebook-fn.txt", "r")
 count = 0
 save_every = 2000
 
-for file in files:
-    f = open(f"namesbystate\{file}", "r")
-    count = 0
-    for line in f:
-        count += 1
-        split = line.split(",")
-        df = df.append({"count":int(split[4]),"name":split[3]}, ignore_index=True)
-        if save_every % count == 0:
-            df = df.groupby(['name']).sum()
-            df.to_csv("namesbystates.csv")
+for line in f:
+    count += 1
+    split = line.split()
+    df = df.append({'count':split[0], 'name':split[1].capitalize()}, ignore_index=True)
+    
+    if count % save_every == 0:
+        df.to_csv("fbfirstnames.csv")
 
-df.groupby(['name']).sum()
-df.to_csv("namesbystates.csv")
+df.to_csv("fbfirstnames.csv")
