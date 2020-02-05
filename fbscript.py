4519a02d8ef54cdf0a19485de299a33d11fd71a4
Dylan Yung <YungDexiong@hotmail.com>
2020-02-05 03:14:16 -0800

    script not names and empty

--- fbscript.py
+++ fbscript.py
@@ -11,6 +11,14 @@ save_every = 2000
 
 for line in f:
     count += 1
+
+    split = line.split()
+
+    if line == '':
+        continue
+    elif len(split) < 2:
+        continue
+
     df = df.append({'count':1, 'name':line}, ignore_index=True)
     
     if save_every % count == 0:
