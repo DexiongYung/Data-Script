9b9cb242c2241ff2b7e47e5c45ccd2491cbca158
Dylan Yung <YungDexiong@hotmail.com>
2020-02-10 23:33:46 -0800

    script to finish

--- script.py
+++ script.py
@@ -16,3 +16,21 @@ for line in f:
         df.to_csv("fbfirstnames.csv")
 
 df.to_csv("fbfirstnames.csv")
+
+
+
+df = pd.DataFrame(columns=['count', 'name'])
+
+f = open("fbnames.txt", "r")
+count = 0
+save_every = 2000
+
+for line in f:
+    count += 1
+    split = line.split()
+    df = df.append({'count':split[0], 'name':split[1].capitalize()}, ignore_index=True)
+    
+    if count % save_every == 0:
+        df.to_csv("fblastnames.csv")
+
+df.to_csv("fblastnames.csv")
