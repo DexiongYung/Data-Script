49f2c68b32efe961db89c57c7807fe9a5a2d8fae
Dylan Yung <YungDexiong@hotmail.com>
2020-02-13 10:42:19 -0800

    last name

--- script.py
+++ script.py
@@ -3,24 +3,6 @@ import os
 
 df = pd.DataFrame(columns=['count', 'name'])
 
-f = open("facebook-fn.txt", "r")
-count = 0
-save_every = 2000
-
-for line in f:
-    count += 1
-    split = line.split()
-    df = df.append({'count':split[0], 'name':split[1].capitalize()}, ignore_index=True)
-    
-    if count % save_every == 0:
-        df.to_csv("fbfirstnames.csv")
-
-df.to_csv("fbfirstnames.csv")
-
-
-
-df = pd.DataFrame(columns=['count', 'name'])
-
 f = open("fbnames.txt", "r")
 count = 0
 save_every = 2000
@@ -31,6 +13,6 @@ for line in f:
     df = df.append({'count':split[0], 'name':split[1].capitalize()}, ignore_index=True)
     
     if count % save_every == 0:
-        df.to_csv("fblastnames.csv")
+        df.to_csv("fb_lastnames.csv")
 
-df.to_csv("fblastnames.csv")
+df.to_csv("fb_lastnames.csv")
